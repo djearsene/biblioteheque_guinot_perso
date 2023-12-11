3fc5ac527bae9c7412143d7bbc9df013d4a9751a
djeArsene <ouanyou21@gmail.com>
2023-12-11 15:22:38 +0100

    ajoue de read.me

---
+++ migrations/Version20231204141028.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231204141028 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, document_id INT DEFAULT NULL, name VARCHAR(500) NOT NULL, INDEX IDX_E01FBE6AC33F7837 (document_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE images ADD CONSTRAINT FK_E01FBE6AC33F7837 FOREIGN KEY (document_id) REFERENCES document (id)');
+        $this->addSql('ALTER TABLE document DROP photo');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE images DROP FOREIGN KEY FK_E01FBE6AC33F7837');
+        $this->addSql('DROP TABLE images');
+        $this->addSql('ALTER TABLE document ADD photo VARCHAR(500) DEFAULT NULL');
+    }
+}
